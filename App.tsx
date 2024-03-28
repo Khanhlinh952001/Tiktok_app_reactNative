1c19437fec74ac9661d9698980c8bfc231d807e0
KhanhLinh <vokhanhlinh952001@gmail.com>
2024-03-28 14:53:10 +0900

    OK

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import TabNavigator from './src/TabNavigator';
+import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ src/videosData.ts
@@ -0,0 +1,72 @@
+export interface VideoModel {
+    id: number;
+    channelName: string;
+    uri: string;
+    caption: string;
+    musicName: string;
+    likes: number;
+    comments: number;
+    avatarUri: string;
+  }
+  
+  export default [
+    {
+      id: 1,
+      channelName: 'cutedog',
+      uri: 'https://v.pinimg.com/videos/mc/720p/f6/88/88/f68888290d70aca3cbd4ad9cd3aa732f.mp4',
+      caption: 'Cute dog shaking hands #cute #puppy',
+      musicName: 'Song #1',
+      likes: 4321,
+      comments: 2841,
+      avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
+    },
+    {
+      id: 2,
+      channelName: 'meow',
+      uri: 'https://v.pinimg.com/videos/mc/720p/11/05/2c/11052c35282355459147eabe31cf3c75.mp4',
+      caption: 'Doggies eating candy #cute #puppy',
+      musicName: 'Song #2',
+      likes: 2411,
+      comments: 1222,
+      avatarUri: 'https://wallpaperaccess.com/thumb/266770.jpg',
+    },
+    {
+      id: 3,
+      channelName: 'yummy',
+      uri: 'https://v.pinimg.com/videos/mc/720p/c9/22/d8/c922d8391146cc2fdbeb367e8da0d61f.mp4',
+      caption: 'Brown little puppy #cute #puppy',
+      musicName: 'Song #3',
+      likes: 3100,
+      comments: 801,
+      avatarUri: 'https://wallpaperaccess.com/thumb/384178.jpg',
+    }, {
+      id: 4,
+      channelName: 'playfulpup',
+      uri: 'https://v.pinimg.com/videos/mc/720p/9d/3d/53/9d3d53c636e5b32acccbe458ac15a785.mp4',
+      caption: 'Playful puppy chasing tail #cute #puppy',
+      musicName: 'Song #4',
+      likes: 5210,
+      comments: 3025,
+      avatarUri: 'https://wallpaperaccess.com/thumb/636178.jpg',
+    },
+    {
+      id: 5,
+      channelName: 'sillypuppy',
+      uri: 'https://v.pinimg.com/videos/mc/720p/cb/a7/67/cba767080fdaee05ee3b05d2f08e03ec.mp4',
+      caption: 'Silly puppy playing with toy #cute #puppy',
+      musicName: 'Song #5',
+      likes: 3800,
+      comments: 2105,
+      avatarUri: 'https://wallpaperaccess.com/thumb/3121867.jpg',
+    },
+    {
+      id: 6,
+      channelName: 'happydog',
+      uri: 'https://v.pinimg.com/videos/mc/720p/39/01/24/390124b3b1f8d8d7d15b6b7df078c087.mp4',
+      caption: 'Happy dog jumping around #cute #puppy',
+      musicName: 'Song #6',
+      likes: 4600,
+      comments: 2800,
+      avatarUri: 'https://wallpaperaccess.com/thumb/6651285.jpg',
+    },
+  ];
